d0c8799c9094302217f13ecaa77069115c1de55b
payalghusalikar <payal.ghusalikar9@gmail.com>
2021-03-07 22:49:59 +0530

    add : loaded the files

--- practice/src/main.js
+++ practice/src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 //import VueRouter from 'vue-router'
 import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import vuetify from './plugins/vuetify';
 
 Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
-    render: h => h(App),
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
